3e6b91b228237fed4861309eda7cf8fb792d27f3
Patrick Nannt <patricknannt@live.com>
2021-03-19 13:24:51 +0000

    Added base configuration for gitpod environment

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 import random
 import csv
 print("hit or shit")
-a=1
-b=2.0
-c=False
-d="1string"
+a = 1
+b = 2.0
+c = False
+d = "1string"
 print(str(a) + ", " + str(type(a)))
 print(b)
 print(c)
 print(d)
 
-def add(v1,v2):
+
+def add(v1, v2):
     return v1*v2
 
-def sub(r,l):
+
+def sub(r, l):
     return r+l
 
-print(sub(1,2))
-print(add("1",2))
+
+print(sub(1, 2))
+print(add("1", 2))
 print(random.random())
 print(csv)
